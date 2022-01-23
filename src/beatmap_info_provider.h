4f5b4ca18144378ced03d9bd56f741ab027d60ed
ShaddyDC <shaddythefirst@gmail.com>
2022-01-23 02:17:29 +0100

    Add nodiscards

--- src/beatmap_info_provider.h
+++ src/beatmap_info_provider.h
@@ -7,28 +7,28 @@ class Beatmap_info_provider {
 public:
     explicit Beatmap_info_provider(const osu::Beatmap& beatmap, osu::Mods mods = {}) : beatmap{beatmap}, mods{mods} {}
 
-    inline float cs() const
+    [[nodiscard]] inline float cs() const
     {
         if(osu::has_mods(mods, osu::Mods::HardRock)) return std::min(beatmap.cs * 1.3f, 10.f);
         if(osu::has_mods(mods, osu::Mods::Easy)) return 0.5f * beatmap.cs;
         return beatmap.cs;
     }
 
-    inline float ar() const
+    [[nodiscard]] inline float ar() const
     {
         if(osu::has_mods(mods, osu::Mods::HardRock)) return std::min(beatmap.ar * 1.4f, 10.f);
         if(osu::has_mods(mods, osu::Mods::Easy)) return 0.5f * beatmap.ar;
         return beatmap.ar;
     }
 
-    inline float od() const
+    [[nodiscard]] inline float od() const
     {
         if(osu::has_mods(mods, osu::Mods::HardRock)) return std::min(beatmap.od * 1.4f, 10.f);
         if(osu::has_mods(mods, osu::Mods::Easy)) return 0.5f * beatmap.od;
         return beatmap.od;
     }
 
-    inline float hp() const
+    [[nodiscard]] inline float hp() const
     {
         if(osu::has_mods(mods, osu::Mods::HardRock)) return std::min(beatmap.hp * 1.4f, 10.f);
         if(osu::has_mods(mods, osu::Mods::Easy)) return 0.5f * beatmap.hp;
--- src/playfield_coordinate_provider.h
+++ src/playfield_coordinate_provider.h
@@ -6,7 +6,7 @@ class Playfield_coordinate_provider {
 public:
     Playfield_coordinate_provider(Magnum::Vector2 offset, bool invert_y) : offset{offset}, invert_y{invert_y} {}
 
-    inline Magnum::Vector2 convert_point(Magnum::Vector2 point) const
+    [[nodiscard]] inline Magnum::Vector2 convert_point(Magnum::Vector2 point) const
     {
         if(invert_y) point.y() = 384.f - point.y();
         return point + offset;
