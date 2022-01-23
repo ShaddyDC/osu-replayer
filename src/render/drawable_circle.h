932582560efb5c0995c64f11a28941fcd9e14126
ShaddyDC <shaddythefirst@gmail.com>
2022-01-23 14:20:31 +0100

    Bind framebuffer explicitly in drawable circle

--- src/render/drawable_circle.h
+++ src/render/drawable_circle.h
@@ -8,8 +8,9 @@ public:
     Drawable_circle(Circleobject_renderer& renderer, Magnum::Vector2 position, float radius, Circle_draw_options options)
         : renderer{renderer}, position{position}, radius{radius}, options{options} {}
 
-    void draw(Magnum::GL::Framebuffer&) override
+    void draw(Magnum::GL::Framebuffer& target) override
     {
+        target.bind();
         auto mesh = renderer.generate_mesh(position, radius);
         renderer.draw(mesh, options);
     }
