fc8e76428dd5c653252fcbe02a8e1c6bf4f92fee
ShaddyDC <shaddythefirst@gmail.com>
2023-02-01 16:55:56 +0100

    Add emscripten build script to flake shell

--- flake.nix
+++ flake.nix
@@ -16,12 +16,22 @@
             openssl
             ninja
           ];
+
+          # Uses podman to ensure same process as CD
+          # Note that this still requires podman to be enabled on the host
+          build-emscripten = pkgs.writeShellScriptBin "build-emscripten" ''
+            ${pkgs.podman}/bin/podman pull ghcr.io/shaddydc/emscripten-magnum-docker:master
+            ${pkgs.podman}/bin/podman run -v "$PWD":/src ghcr.io/shaddydc/emscripten-magnum-docker:master sh -c "
+              cd /src && cd build-emscripten &&
+              cmake .. -DCMAKE_TOOLCHAIN_FILE="../toolchains/generic/Emscripten-wasm.cmake" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=./deploy &&
+              cmake --build . --target install"
+          '';
         in
         with pkgs;
         {
           devShells.default = stdenv.mkDerivation {
             name = "dev-shell";
-            buildInputs = inputs;
+            buildInputs = inputs ++ [ build-emscripten ];
           };
         }
       );
